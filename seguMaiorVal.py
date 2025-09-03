96b98117d410106bb8c0719fcc81ec7bffc3d3d6
periclesjunior <>
2025-09-03 09:10:08 -0300

    encontre o segundom,aior numero

---
+++ seguMaiorVal.py
@@ -0,0 +1,9 @@
+lista = ["8", "3", "6", "2", "5", "10", "4"]
+segunda_maior = "8"
+for numero in lista:
+    if numero > segunda_maior:
+        segunda_maior = numero
+print(f"A segunda maior valor é: {segunda_maior}")
+print(f"A lista é: {lista}")
+
+ 
